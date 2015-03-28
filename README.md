81df492920104061961d45b41a4b3ab89003093e
M. David Bennett <mdavidbennett@syntheticworks.com>
2015-03-28 08:29:55 -0500

    Initial version
    Wrote v2.0 plugin for Rackspace

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # python-keystoneclient-rackspaceauth
 Plugin for API key authentication in the Rackspace Cloud
+
+Example usage:
+    from keystoneclient_rackspaceauth.v2_0 import RackspaceAuth
+    from keystoneclient import session
+    from novaclient.client import Client
+
+    auth = RackspaceAuth(
+        auth_url="https://identity.api.rackspacecloud.com/v2.0",
+        username="demoauthor",
+        api_key="aaaaa-bbbbb-ccccc-12345678")
+    session = session.Session(auth=auth)
+    client = Client(2, session=session, region_name="DFW")
+    client.servers.list()
---
+++ setup.py
@@ -0,0 +1,49 @@
+# Copyright 2011 OpenStack, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import setuptools
+
+
+def read_file(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setuptools.setup(
+    name="python-keystoneclient-rackspaceauth",
+    version="0.1",
+    author="M. David Bennett",
+    author_email="mdavidbennett@syntheticworks.com",
+    description="Rackspace Auth Plugin for OpenStack Clients.",
+    long_description=read_file("README.md"),
+    license="Apache License, Version 2.0",
+    url="https://github.com/testeddoughnut/python-keystoneclient-rackspaceauth",
+    install_requires=['python-keystoneclient'],
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*', 'test_*']),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Environment :: OpenStack",
+        "Programming Language :: Python"
+    ],
+    entry_points={
+        "keystoneclient.auth.plugin": [
+            "rackspace = keystoneclient_rackspace.v2_0:RackspaceAuth"
+        ]
+    }
+)
